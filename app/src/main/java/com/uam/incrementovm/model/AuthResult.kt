3612423aa52dc35baa0000b6f5caa2de470ffa03
normancash <normancashnic@gmail.com>
2025-06-17 18:56:51 -0600

    Commit final

--- app/src/main/java/com/uam/incrementovm/model/AuthResult.kt
+++ app/src/main/java/com/uam/incrementovm/model/AuthResult.kt
@@ -1,7 +1,6 @@
 package com.uam.incrementovm.model
 
 sealed class AuthResult {
-    object Idle : AuthResult()
     object Loading : AuthResult()
     data class Success(val user : User) : AuthResult()
     data class Error(val message:String): AuthResult()
