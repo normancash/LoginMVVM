0342018a82296da0a85813f1bce37ae15f0b086c
Adan909 <jorgeot@uamv.edu.ni>
2025-06-19 17:58:07 -0600

    Final commit

--- app/src/main/java/com/uam/incrementovm/model/AuthResult.kt
+++ app/src/main/java/com/uam/incrementovm/model/AuthResult.kt
@@ -1,6 +1,7 @@
 package com.uam.incrementovm.model
 
 sealed class AuthResult {
+    object Idle : AuthResult()
     object Loading : AuthResult()
     data class Success(val user : User) : AuthResult()
     data class Error(val message:String): AuthResult()
