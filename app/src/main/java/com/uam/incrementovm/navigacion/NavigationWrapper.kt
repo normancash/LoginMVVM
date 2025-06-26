cedeb0212be6a160f293d84111633d73eb8c152f
normancash <normancashnic@gmail.com>
2025-06-26 17:50:59 -0600

    Final commit

--- app/src/main/java/com/uam/incrementovm/navigacion/NavigationWrapper.kt
+++ app/src/main/java/com/uam/incrementovm/navigacion/NavigationWrapper.kt
@@ -5,9 +5,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.uam.incrementovm.model.User
 import com.uam.incrementovm.screen.DetailUserScreen
 import com.uam.incrementovm.screen.LoginScreen
 import com.uam.incrementovm.screen.UserScreen
+import com.uam.navegacionobject.type.generarType
+import kotlin.reflect.typeOf
 
 
 @Composable
@@ -19,24 +22,13 @@ fun Navigation() {
         }
         composable<ListUser>()
         { backStackEntry ->
-            UserScreen(navController.navigate(DetailUser(it)))
+            UserScreen {navController.navigate(DetailUser(it))}
         }
-        composable<DetailUser>(){
+        composable<DetailUser>(typeMap = mapOf(typeOf<User>() to generarType<User>())){
             backStackEntrty ->
-            DetailUserScreen()
+            val detail : DetailUser = backStackEntrty.toRoute()
+            DetailUserScreen(detail.user)
         }
     }
 }
 
-/*
-NavHost(navController = navController, startDestination = Home) {
-        composable<Home>{backStackEntry ->
-          HomeScreen(){navController.navigate(Detail(it))}
-        }
-        composable<Detail>(typeMap = mapOf(typeOf<User>() to generarType<User>()))
-        { backStackEntry ->
-            val detail : Detail = backStackEntry.toRoute()
-            DetailScreen(detail.user)
-        }
-    }
- */
